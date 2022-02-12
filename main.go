e5ca793b37800e92b9dfc34ff53a46a7ff29ccdf
Navratan Gupta <navilg0409@gmail.com>
2022-02-12 22:05:03 +0530

    Check if dir exist

--- main.go
+++ main.go
@@ -17,6 +17,12 @@ func main() {
 	path := flag.String("path", currentDir, "Path to serve")
 	port := flag.String("port", "3000", "Port to listen on")
 	flag.Parse()
+
+	if _, err := os.Stat(*path); os.IsNotExist(err) {
+		fmt.Println(err.Error())
+		return
+	}
+
 	fmt.Printf("Serving files under %v on port %v \n", *path, *port)
 	// http.HandleFunc("/", root)
 	http.Handle("/", http.FileServer(http.Dir(*path)))
